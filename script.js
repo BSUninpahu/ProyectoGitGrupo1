852507af51240cd31a9a5040672722f5ebcf4c00
dAfTDevBranch <dfajardoto@uninpahu.edu.co>
2024-02-29 17:41:07 -0500

    Correcciones finales.

--- script.js
+++
@@ -1,8 +0,0 @@
-const slider = document.getElementById('slider');
-const image = document.getElementById('image');
-
-slider.addEventListener('input', function() {
-    const value = this.value;
-    image.style.width = value + 'px';
-    image.style.height = value + 'px';
-});
