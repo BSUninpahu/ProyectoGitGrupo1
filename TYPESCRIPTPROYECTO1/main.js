9f3231a6665407071951173aad7b19e1ac4fccfd
SSalazarGit <liblie919@gmail.com>
2024-02-29 11:05:53 -0500

    Selector de color y login eliminados

--- TYPESCRIPTPROYECTO1/main.js
+++ TYPESCRIPTPROYECTO1/main.js
@@ -30,8 +30,3 @@ document.addEventListener("DOMContentLoaded", function () {
   
     });
   });
-  function cambiarColor() {
-    var color = document.getElementById("colorInput").value;
-    document.body.style.backgroundColor = color;
-  }
-  
